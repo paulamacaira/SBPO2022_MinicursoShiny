80bc5b032fc463a1329a5755b786a8269d0f2cdd
Paula Macaira <paulamacaira@gmail.com>
2022-11-10 19:56:44 -0300

    vfinal

---
+++ appDygraphs.R
@@ -0,0 +1,36 @@
+library(shiny)
+library(dygraphs)
+
+ui <- fluidPage(
+  titlePanel("Predicted Deaths from Lung Disease (UK)"),
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("months", label = "Months to Predict", 
+                   value = 72, min = 12, max = 144, step = 12),
+      selectInput("interval", label = "Prediction Interval",
+                  choices = c("0.80", "0.90", "0.95", "0.99"),
+                  selected = "0.95"),
+      checkboxInput("showgrid", label = "Show Grid", value = TRUE)
+    ),
+    mainPanel(
+      dygraphOutput("dygraph")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  predicted <- reactive({
+    hw <- HoltWinters(ldeaths)
+    predict(hw, n.ahead = input$months, 
+            prediction.interval = TRUE,
+            level = as.numeric(input$interval))
+  })
+  
+  output$dygraph <- renderDygraph({
+    dygraph(predicted(), main = "Predicted Deaths/Month") %>%
+      dySeries(c("lwr", "fit", "upr"), label = "Deaths") %>%
+      dyOptions(drawGrid = input$showgrid)
+  })
+}
+
+shinyApp(ui, server)
